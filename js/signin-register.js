1ce908d7141172765d54824fbe9bce478e4108f5
hightechu2 <hightechu2@uvic.ca>
2019-05-25 16:02:41 -0700

    adding signin functionality

--- js/signin-register.js
+++ js/signin-register.js
@@ -1,9 +1,82 @@
 //save signin infomation
-function saveSignin(){
-    var username = document.getElementById("Username").innerHTML;
-    var email = "";
-    var password = "";
-    var confirmPassword = "";
+//saveSignin variables
 
-    alert(username);
+var emailId = "";
+var username = "";
+var password = "";
+var confirmPassword = "";
+
+
+
+function saveSignin(){
+
+    emailId = document.getElementById("Email").value;
+    username = document.getElementById("signinUsername").value;
+    password = document.getElementById("Password").value;
+    confirmPassword = document.getElementById("ConfirmPassword").value;
+
+    var regex = /^(\w+@)([a-z]+)(\.)([a-z]{1,5})$/;
+    var checkEmail = regex.test(emailId);
+
+    /*if(password != confirmPassword){
+        document.getElementById("ConfirmPassword").value = null;
+        document.getElementById("ConfirmPassword").placeholder = "The passwords do not match";
+    }*/
+
+    if (checkEmail && (password == confirmPassword)) {
+        alert("Signup successful");
+    }
+    
+}
+
+function checkConfirmPassword(){
+    var password = document.getElementById("Password").value;
+    var confirmPassword = document.getElementById("ConfirmPassword").value;
+        
+
+    if(password == confirmPassword) {
+        document.getElementById("ConfirmPassword").style.backgroundColor = '';
+    }
+    else {
+        document.getElementById("ConfirmPassword").style.backgroundColor = 'tomato';
+        document.getElementById("ConfirmPassword").value = null;
+        document.getElementById("ConfirmPassword").placeholder = "The passwords do not match";
+    }
+}
+
+function checkEmailAddress() {
+    var emailId = document.getElementById("Email").value;
+    var regex = /^(\w+@)([a-z]+)(\.)([a-z]{1,5})$/;
+
+    var checkEmail = regex.test(emailId);
+
+    if(checkEmail) {
+        document.getElementById("Email").style.backgroundColor = '';
+    }
+    else {
+        document.getElementById("Email").style.backgroundColor = 'tomato';
+    }
+}
+
+
+//check login
+function login(){
+    
+    //login variables
+    var loginName = document.getElementById("Email/Username").value;
+    var loginPassword = document.getElementById("LoginPassword").value;
+
+    //emailId = document.getElementById("Email").value;
+    //username = document.getElementById("signinUsername").value;
+    //password = document.getElementById("Password").value;
+
+    if((loginName == username || loginName == emailId) && (loginPassword == password || loginPassword == confirmPassword)) {
+        alert("yay");
+    }
+    
+    else{
+        document.getElementById("Email/Username").placeholder = "Invalid Login Name.";
+        document.getElementById("LoginPassword").placeholder = "Invalid Login Pass."
+    }
+
 }
